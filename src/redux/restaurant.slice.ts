5f97ccd2ea02b40b289645eb204c6177446e2db8
tush2890 <traj490@gmail.com>
2023-10-04 07:34:36 +0100

    added two more state properties for restaurant

--- src/redux/restaurant.slice.ts
+++ src/redux/restaurant.slice.ts
@@ -1,31 +1,47 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 import { Restaurant } from "../utils/models";
 
-const initialState: Restaurant = {
-    address: { city: '', country: '', postcode: '', state: '', streetAddress: '' },
-    category: [],
-    id: '',
-    items: [],
-    name: '',
-    rating: 0,
-    isOpen: false
+interface RestaurantState {
+    restaurant: Restaurant;
+    restaurants: Restaurant[];
+    searchParam: string;
+}
+
+const initialState: RestaurantState = {
+    restaurant: {
+        address: { city: '', country: '', postcode: '', state: '', streetAddress: '' },
+        category: [],
+        id: '',
+        items: [],
+        name: '',
+        rating: 0,
+        isOpen: false
+    },
+    restaurants: [],
+    searchParam: ''
 }
 
 export const RestaurantsSlice = createSlice({
     name: 'restaurant',
     initialState,
     reducers: {
-        setRestaurant: (restaurantState, action: PayloadAction<{ restaurant: Restaurant }>) => {
-            restaurantState.address = action.payload.restaurant.address;
-            restaurantState.category = action.payload.restaurant.category;
-            restaurantState.id = action.payload.restaurant.id;
-            restaurantState.items = action.payload.restaurant.items;
-            restaurantState.name = action.payload.restaurant.name;
-            restaurantState.rating = action.payload.restaurant.rating;
-            restaurantState.isOpen = action.payload.restaurant.isOpen;
+        setRestaurant: (restState, action: PayloadAction<{ restaurant: Restaurant }>) => {
+            restState.restaurant.address = action.payload.restaurant.address;
+            restState.restaurant.category = action.payload.restaurant.category;
+            restState.restaurant.id = action.payload.restaurant.id;
+            restState.restaurant.isOpen = action.payload.restaurant.isOpen;
+            restState.restaurant.items = action.payload.restaurant.items;
+            restState.restaurant.name = action.payload.restaurant.name;
+            restState.restaurant.rating = action.payload.restaurant.rating;
+        },
+        populateRestaurants: (restState, action: PayloadAction<{ restaurants: Restaurant[] }>) => {
+            restState.restaurants = action.payload.restaurants;
+        },
+        setSearchParam: (restState, action: PayloadAction<{ searchParam: string }>) => {
+            restState.searchParam = action.payload.searchParam;
         }
     }
 });
 
 export default RestaurantsSlice.reducer;
-export const { setRestaurant } = RestaurantsSlice.actions
+export const { setRestaurant, populateRestaurants, setSearchParam } = RestaurantsSlice.actions
